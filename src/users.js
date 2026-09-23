0e79310e4c5101bcf52652488f94366dd54109ac
RumeshChathuranga <rumeshc.23@cse.mrt.ac.lk>
2026-09-23 15:07:52 +0530

    fix: remove eval, unused vars, deep nesting and duplication

--- src/users.js
+++ src/users.js
@@ -1,20 +1,8 @@
 // User formatting helpers.
 
 function formatUser(user) {
-  const unusedPrefix = "user:";
-
-  if (user) {
-    if (user.profile) {
-      if (user.profile.name) {
-        if (user.profile.name.first) {
-          if (user.profile.name.first.length > 0) {
-            return user.profile.name.first;
-          }
-        }
-      }
-    }
-  }
-  return "anonymous";
+  const first = user?.profile?.name?.first;
+  return first && first.length > 0 ? first : "anonymous";
 }
 
 module.exports = { formatUser };
